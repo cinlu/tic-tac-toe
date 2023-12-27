69256cd55cb615655a75366a9819f35c0be24847
Cindy <19cindyl@gmail.com>
2023-12-27 18:41:42 -0500

    added github repo link to footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Footer from './Components/Footer';
 import TicTacToe from './Components/TicTacToe';
 
 function App() {
   return (
     <div>
       <TicTacToe/>
-      <footer className='footer-message'> cozy games to play on your own or with friends </footer>
+      <Footer/>
     </div>
   );
 }
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className='footer-message'>
+      <a href="https://github.com/cinlu/tic-tac-toe"> go to github repo</a>
+    </footer>
+  );
+}
+
+export default Footer;
